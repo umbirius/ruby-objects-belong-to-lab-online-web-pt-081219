d63efd49345378f3cbdd0d3b6c50bf245d91366f
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-16 21:27:35 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Post 
+  
+  attr_accessor :author, :title
+  
+  # def initialize(title)
+  #   @title = title 
+  # end 
+  
+
+  
+end 
+    
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+  
+  attr_accessor :title, :artist
+  
+  # def initialize(title)
+  #   @title = title
+  # end 
+  
+  # def title
+  #   @title = title 
+  # end 
+  
+  # def artist(artist) 
+  #   @artist = artist 
+  # end 
+  
+  
+  
+end 
