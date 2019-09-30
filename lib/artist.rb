9c21c5e419740399d93f84b6839de5769deb856d
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-30 19:39:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,12 +2,12 @@ class Artist
   
   attr_accessor :name
   
-  # def initialize(name)
-  #   @name = name 
-  # end 
+  def initialize(name)
+    @name = name 
+  end 
   
-  # def name 
-  #   @name 
-  # end 
+  def name 
+    @name 
+  end 
 
 end 
